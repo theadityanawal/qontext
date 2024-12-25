d49035205cabe1367b0860a6d7f0d21809a6e20c
Aditya Nawal <nawaladitya7.2@gmail.com>
2024-12-26 02:20:59 +0530

    Add vercel analytics and speed insights

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,9 @@
 // src/app/page.tsx
+import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/next"
+
+
+
 export default function Home() {
     return (
       <main className="flex min-h-screen flex-col items-center justify-between p-24">
