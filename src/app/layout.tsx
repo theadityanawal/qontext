65e7985f98fc1d008f7bc3b50d21a84b39e60eba
Aditya Nawal <nawaladitya7.2@gmail.com>
2024-12-26 02:23:48 +0530

    Add head and body in app layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,17 @@
-// Add your imports here
 import React from 'react';
 
-// Your component code
 interface LayoutProps {
   children: React.ReactNode;
 }
 
 const Layout: React.FC<LayoutProps> = ({ children }) => {
   return (
-    <div>
-      {children}
-    </div>
+    <html>
+      <body>
+        {children}
+      </body>
+    </html>
   );
 };
 
-// Export the component to make this file a module
 export default Layout;
